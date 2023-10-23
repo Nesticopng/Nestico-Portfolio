405767f713efb580b5f8017beef525e5166fc522
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2023-10-23 08:05:09 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Nestico.png Portfolio',
+  title: 'Nestico.png | Portfolio',
   description: 'Hello! I&#39;m Nestico, a dedicated programmer in constant evolution. Welcome to my digital corner, where technology comes to life. Explore my portfolio and discover my journey through the world of development.',
 }
 
