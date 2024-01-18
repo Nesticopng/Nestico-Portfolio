c5d6a442196f27558d0c3ad6ad529dbd2dc0d5bc
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2024-01-18 04:24:31 -0400

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -44,7 +44,7 @@ const EmailSection = () => {
           <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
           <p className='text-[#ADB7BE] mb-4 max-w-md'>
             {" "}
-            I'm currently looking for new oppornunities, my inbox is always open.
+            I'm currently looking for new opportunities, my inbox is always open.
             Whether you have a question or just want to say hi, I'll try my best
             to get back to you!
           </p>
