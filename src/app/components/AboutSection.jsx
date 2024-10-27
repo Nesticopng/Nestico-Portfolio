15dc6c8f57ff0ed9e9a014e34ce82897d26e3ca8
Nestor Cano <126262899+Nesticopng@users.noreply.github.com>
2024-10-27 18:28:53 -0400

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -45,7 +45,7 @@ const AboutSection = () => {
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
                 <Image src="/images/About-Image.png" width={500} height={500} alt=''/>  
                 <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
-                    <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
+                    <h2 className='text-4xl font-bold text-white mb-4'>Sobre Mí</h2>
                     <p className="text-base lg:text-lg">
 Soy un desarrollador web full stack con pasión por crear aplicaciones web interactivas y optimizadas. Tengo experiencia trabajando con JavaScript, Node.js, React, Next.js, TypeScript, Express, Python y Git. Aprendo rápido y siempre busco expandir mis conocimientos y habilidades. Me gusta trabajar en equipo y colaborar con otros para crear aplicaciones increíbles.
                     </p>
